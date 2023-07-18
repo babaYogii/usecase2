ef6c4846e672b60bd3871271fc64d18b7ab12ab1
babaYogii <yogeshkodlinge@gamil.com>
2023-07-18 17:15:02 +0530

    Add dot env

--- backend/connection.js
+++ backend/connection.js
@@ -1,6 +1,8 @@
 const mongoose=require('mongoose')
+require("dotenv").config()
 
-const url="mongodb://yogesh:a8HF0dbf8kmOEFsmxdmc0Xt9V31o3kabLUzbeQluxbJxUO4AgLIHbP4ZMP5wqtD2VW4gh2BGe9MfACDboEGzPw==@yogesh.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@yogesh@"
+
+const url=process.env.DB_URL;
 // const url="mongodb+srv://yogeshkodlinge:Yogesh123@cluster0.ymuefl6.mongodb.net/";
 
 
