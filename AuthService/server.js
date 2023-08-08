c4b03480ed222a6be6ce1b19bc99cf41bf144cd3
babaYogii <yogeshkodlinge@gamil.com>
2023-08-08 07:40:53 +0530

    Authservice changes

--- AuthService/server.js
+++ AuthService/server.js
@@ -1,11 +1,3 @@
-const express=require('express');
-const app=express();
-const cors=require('cors')
-const userRoute=require('./Router/userRoute');
-// const employeeRoute=require('./Router/employeeRoutes')
-// const path=require('path')
-
-
 var apm = require('elastic-apm-node').start({
     serviceName: 'Auth-service',
     secretToken: '2RvuKXH7g2KNkGEoNq',
@@ -13,12 +5,23 @@ var apm = require('elastic-apm-node').start({
     environment: 'my-environment'
 })
 
+
+const express=require('express');
+const app=express();
+const cors=require('cors')
+const userRoute=require('./Router/userRoute');
+// const employeeRoute=require('./Router/employeeRoutes')
+app.use(apm.middleware.connect());
+// const path=require('path')
+
+
+
+
 const connectDb = require('./connection');
 
 app.use(cors());
 app.use(express.json());
 app.use('/auth',userRoute)
-app.use(apm.middleware.connect());
 // app.use(employeeRoute)
 
 
